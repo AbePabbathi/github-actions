75635de024b48eb3fe53bb77191569b9cac299c7
abraham.pabbathi@databricks.com <abraham.pabbathi@databricks.com>
2022-10-26 14:05:59 +0000

    News Changes 10/26 9:05

--- etl-functions/auditFields/addDates.py
+++ etl-functions/auditFields/addDates.py
@@ -1,6 +1,6 @@
 from pyspark.sql.functions import lit
 from datetime import datetime
-# add load date 
+# add load date functions
 def addLoadDate(df):
     df = df.withColumn("_loadDate",lit(datetime.now()))
     return df
